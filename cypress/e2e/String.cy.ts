c57fbe1a677c4f9d2b43cb7a306a8c04e4ffeec6
YuraNotebook <dudinyv@yandex.ru>
2023-03-23 17:58:28 +0500

    add cypress test string

--- cypress/e2e/String.cy.ts
+++ cypress/e2e/String.cy.ts
@@ -1,3 +1,10 @@
+import { DELAY_IN_MS } from '../../src/constants/delays';
+import { permutation } from "../../src/utils/algorithms/string";
+
+const stringTest = '12345';
+const newMas = permutation(stringTest.split(''));
+console.log("newMas", newMas);
+
 describe('Строка', () => {
   beforeEach(() => {
     cy.visit('/recursion')
@@ -9,17 +16,51 @@ describe('Строка', () => {
   })
 
   it('Проверьте, что строка разворачивается корректно', () => {
-    cy.get('input[name="text"]').type('12345').should('have.value', '12345')
+    cy.get('input[name="text"]').type('12345').should('have.value', stringTest)
     cy.get('button[type="submit"]').click()
     cy.get('input[name="text"]').should('have.value', '')
     cy.get('button[type="submit"]').should('be.disabled')
 
-    const t = cy.get('*[class^="circle_circle"]')
-    console.log("tttttt = ", t)
-    t.each((item, index) => {
-      console.log("item = ", item)
-      item[0].contains('circle_default')
-      cy.get('item[0]').contains('have.class', 'circle_default')
-    })
+    // const elements = cy.get('*[class^="circle_circle"]')
+    // console.log("elements", elements);
+
+    let i = 0;
+    while (i < newMas.length) {
+      const elements = cy.get('*[class^="circle_circle"]')
+      elements.each((item, index) => {
+        // if (index === newMas[i].)
+        console.log("index", index);
+        cy.wrap(item)
+          .invoke('attr', 'class')
+          .then(classList => expect(classList).contains('circle_default'))
+      })
+
+      // elements.each((item) => {
+      //   cy.wrap(item)
+      //   .invoke('attr', 'class')
+      //   .then(classList => expect(classList).contains('circle_default'))
+      // })
+      // cy.wait(DELAY_IN_MS)
+
+      i++;
+    }
+
+    // elements.each((item) => {
+    //   cy.wrap(item)
+    //   .invoke('attr', 'class')
+		// 	.then(classList => expect(classList).contains('circle_default'))
+    // })
+
+    // cy.wait(DELAY_IN_MS)
+
+    // elements.each((item, index) => {
+    //   console.log("item" , item.length);
+    //   if(index === 0 || index === stringTest.length-1){
+    //     cy.wrap(item)
+    //     .invoke('attr', 'class')
+    //     .then(classList => expect(classList).contains('circle_changing'))
+    //   }
+    // })
+
   })
 })
